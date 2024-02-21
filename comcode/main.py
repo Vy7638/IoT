38e231ec6703fb4d113f50cc6bd11ddbf2d8da06
Vy <vy.tranngoc@hcmut.edu.vn>
2024-02-21 19:51:27 +0700

    Init lab 2 simple_ai

--- comcode/main.py
+++ comcode/main.py
@@ -2,10 +2,11 @@ import sys
 from Adafruit_IO import MQTTClient
 import time
 import random
+from simple_ai import image_detector
 
-AIO_FEED_ID = ["led", "temperature", "light", "humidity"]
+AIO_FEED_ID = ["led", "temperature", "light", "humidity", "status-ai"]
 AIO_USERNAME = "Vy2908"
-AIO_KEY = ""
+AIO_KEY = "aio_jLZA29mrbdAjM8Y76PyRjvIGWz4d"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -31,20 +32,28 @@ client.connect()
 client.loop_background()
 counter = 10
 led = 0;
+res_ai = ""
+
 while True:
     counter = counter - 1
     if counter <= 0 :  
         counter = 10
-        temp = random.randint(20, 50)
-        client.publish("temperature", temp)
-        light = random.randint(0, 5000)
-        client.publish("light", light)
-        humi = random.randint(0, 100)
-        client.publish("humidity", humi)
-        led1 = random.randint(0,1)
-        if led1 != led :
-            led = led1;
-            client.publish("led", led)
+        # temp = random.randint(20, 50)
+        # client.publish("temperature", temp)
+        # light = random.randint(0, 5000)
+        # client.publish("light", light)
+        # humi = random.randint(0, 100)
+        # client.publish("humidity", humi)
+        # led1 = random.randint(0,1)
+        # if led1 != led :
+        #     led = led1;
+        #     client.publish("led", led)
+        res_ai_prev = res_ai
+        res_ai = image_detector()
+        print("Output AI: ", res_ai)
+        if res_ai_prev != res_ai:
+            client.publish("status-ai", res_ai)
+
     time.sleep(1)        
 
     
